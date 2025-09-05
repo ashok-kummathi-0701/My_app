df781239afc361a640561dcd560b3867caad0b04
Kummathi Ashok <kummathiashok6301@gmail.com>
2025-09-05 13:30:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 My Hello World app')
+st.title("Hello, Streamlit!")
 
-st.write('Hello world!')
+
+st.subheader("st.column")
+with st.sidebar:
+    st.header("About app")
+    st.write("This is my first app!")
+
+st.header("This is a simple Streamlit app.", divider="rainbow")
+st.markdown("This is created by using st.markdown")
+col1, col2 = st.columns(2)
+with col1:
+    x = st.slider("Choose an x value",1,10)
+with col2:
+    st.write("The value of :blue[**x**] is", x)
+
+st.subheader("st.area_chart")
+
+df = pd.DataFrame(np.random.standard_normal((20, 3)), columns=["a", "b", "c"])
+
+st.area_chart(df)
